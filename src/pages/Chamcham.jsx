9aeea63fe127e0470598e95476a76adcc45e7eb6
Kim-yujin <w2117@e-mirim.hs.kr>
2023-05-14 23:54:40 +0900

    Add: 말풍선

--- src/pages/Chamcham.jsx
+++ src/pages/Chamcham.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import Frame from '../components/Frame';
 import Spacemotion from '../image/spacemotion.gif'
 import AnimatedGif from '../components/AnimatedGif ';
-
+import titletext from '../image/titletext.png'
 function Chamcham(){
 	return (
 			<>
 				{/* <Proverb /> */}
 				<Frame color={"#000000"} />
-				<div style={{textAlign:'center',}}>
-					{/* <img src={Spacemotion} style={{width:909, height:621, zIndex:-3, marginTop: 590}}/> */}
-					<AnimatedGif />
+				<div style={{height: 330}}></div>
+				<div style={{textAlign:'center'}}>
+					<div style={{width: 837, height: 373, backgroundImage : `url(${titletext})`, marginLeft: 100, color: 'black'}}>
+						dsfdsfsfsdfsdsdfdsf
 					</div>
+					<AnimatedGif />
+				</div>
 			</>
 		);
 }
