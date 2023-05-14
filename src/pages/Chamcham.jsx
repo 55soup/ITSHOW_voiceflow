e987e716e8f33e4874c676f784f864a14fb68581
Kim-yujin <w2117@e-mirim.hs.kr>
2023-05-14 23:24:53 +0900

    Add: Gif 무한 로딩

--- src/pages/Chamcham.jsx
+++ src/pages/Chamcham.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Frame from '../components/Frame';
 import Spacemotion from '../image/spacemotion.gif'
+import AnimatedGif from '../components/AnimatedGif ';
+
 function Chamcham(){
 	return (
 			<>
 				{/* <Proverb /> */}
 				<Frame color={"#000000"} />
-				<div style={{textAlign:'center',}}><img src={Spacemotion} style={{width:838, height:621, zIndex:-3}}/></div>
+				<div style={{textAlign:'center',}}>
+					{/* <img src={Spacemotion} style={{width:909, height:621, zIndex:-3, marginTop: 590}}/> */}
+					<AnimatedGif />
+					</div>
 			</>
 		);
 }
