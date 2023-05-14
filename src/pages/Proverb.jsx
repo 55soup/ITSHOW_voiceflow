effb870e927650be94e245260cb665454d935431
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-05-15 00:42:51 +0900

    Fixed: 속담 정답 맞춘 후 다음 문제로

--- src/pages/Proverb.jsx
+++ src/pages/Proverb.jsx
@@ -27,7 +27,7 @@ function PRoverb() {
     "외양간 고친다",
     "부뚜막에 올라간다",
     "바위 치기",
-    "올챙이 적 생각 못 한다",
+    "올챙이 적 기억 못 한다",
     "겨 묻은 개 나무란다",
     "개구리",
     "팥 심은데 팥 난다",
@@ -44,22 +44,62 @@ function PRoverb() {
   let [idx, setIdx] = useState(0);
   let [count, setCount] = useState(0);
   let [correct, setCorrect] = useState(false);
-
-  useEffect(() => {
-    setCorrect(proverbBack[idx] === value);
-    if (correct) {
-      console.log(correct);
-      setValue("");
-      // proverbBack[idx] === value 가 true => 정답, 다음문제로 넘어감(idx가 바뀜. 인식)
-      setIdx(Math.floor(Math.random() * (proverbFront.length - 1)) + 1);
-      setCorrect(false);
-      setCount(count + 1);
-      // console.log(`${correct} ${idx} 값이 바뀌었음.`);
-    }
-  }, [value, idx]);
-
   // toggle버튼 관리
   const [toggle, setToggle] = useState(false);
+
+  // useEffect(() => {
+  //   if (proverbBack[idx] === value) {
+  //     // 말한 답이 정답과 일치하는가?
+  //     setCorrect(true);
+  //     setTimeout(() => {
+  //       // 문제를 다 맞춘 후 correct false로 바뀜
+  //       setCorrect(false);
+  //     }, 2000);
+  //   }
+  // }, [value]);
+
+  // useEffect(() => {
+  //   if (correct) {
+  //     // 2초 뒤에 문제바뀜.
+  //     setTimeout(() => {
+  //       setValue("");
+  //       // proverbBack[idx] === value 가 true => 정답, 다음문제로 넘어감(idx가 바뀜. 인식)
+  //       setIdx(Math.floor(Math.random() * (proverbFront.length - 1)) + 1);
+  //       setCount(count + 1);
+  //       // console.log(`${correct} ${idx} 값이 바뀌었음.`);
+  //     }, 2000);
+  //   }
+  // }, [correct]);
+
+  useEffect(() => {
+    if (proverbBack[idx] === value) {
+      // 말한 답이 정답과 일치하는가?
+      setCorrect(true);
+    }
+  }, [idx, value]);
+
+  useEffect(() => {
+    if (correct) {
+      // 2초 후 => (대답 비우기, 문제 바꾸기, count +1, correct -> false)
+      const timeoutId = setTimeout(() => {
+        setValue("");
+        // proverbBack[idx] === value 가 true => 정답, 다음문제로 넘어감(idx가 바뀜. 인식)
+        setIdx((prevIdx) => {
+          let nextIdx =
+            Math.floor(Math.random() * (proverbFront.length - 1)) + 1;
+          while (nextIdx === prevIdx) {
+            // 새로운 문제가 이전 문제와 동일한 경우, 새로운 문제 다시 뽑기.
+            nextIdx = Math.floor(Math.random() * (proverbFront.length - 1)) + 1;
+          }
+          return nextIdx;
+        });
+        setCount((prevCount) => prevCount + 1);
+        setCorrect(false); // 맞춘 후 다시 correct -> false로
+      }, 2000);
+      // 이전에 등록한 timeout을 clear 하기 위해 return 하여 clear 함수를 등록합니다.
+      return () => clearTimeout(timeoutId);
+    }
+  }, [correct, proverbFront.length]);
 
   const clickedToggle = () => {
     setToggle((prev) => !prev);
@@ -73,16 +113,12 @@ function PRoverb() {
     <>
       <Container>
         <SpeechBubble rotate={"rotate(180deg)"}>
-          <SpeechText rotate={"rotate(180deg)"} padding={"7.2rem"}>
-            {correct
-              ? proverbFront[
-                  Math.floor(Math.random() * (proverbFront.length - 1)) + 1
-                ]
-              : proverbFront[idx]}
+          <SpeechText rotate={"rotate(180deg)"} padding={"13vw"}>
+            {proverbFront[idx]}
           </SpeechText>
         </SpeechBubble>
         <SpeechBubble rotate={"rotate(0deg)"}>
-          <SpeechText rotate={"rotate(0deg)"} padding={"2.8rem"}>
+          <SpeechText rotate={"rotate(0deg)"} padding={"5vw"}>
             {value}
           </SpeechText>
         </SpeechBubble>
@@ -119,8 +155,8 @@ const Container = styled.div`
 `;
 
 const SpeechBubble = styled.div`
-  width: 45rem;
-  height: 13rem;
+  width: 200vw;
+  height: 22vw;
   background-image: url("images/speechbubble.png");
   background-position: center;
   background-size: contain;
@@ -132,7 +168,7 @@ const SpeechBubble = styled.div`
 
 const SpeechText = styled.div`
   padding-top: ${(props) => props.padding};
-  font-size: 3rem;
+  font-size: 4.5vw;
   transform: ${(props) => props.rotate};
 `;
 
