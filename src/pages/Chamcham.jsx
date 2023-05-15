ccab07bd1516bdd95e74ba3f1d6f7e26e06ab3ab
55soup <w2118@e-mirim.hs.kr>
2023-05-15 09:34:14 +0900

    Add: 배경 gif -> mp4 무한재생

--- src/pages/Chamcham.jsx
+++ src/pages/Chamcham.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
-import Frame from '../components/Frame';
-import Spacemotion from '../image/spacemotion.gif'
-import AnimatedGif from '../components/AnimatedGif ';
-import titletext from '../image/titletext.png'
-function Chamcham(){
-	return (
-			<>
-				{/* <Proverb /> */}
-				<Frame color={"#000000"} />
-				<div style={{height: 330}}></div>
-				<div style={{textAlign:'center'}}>
-					<div style={{width: 837, height: 373, backgroundImage : `url(${titletext})`, marginLeft: 100, color: 'black'}}>
-						dsfdsfsfsdfsdsdfdsf
-					</div>
-					<AnimatedGif />
-				</div>
-			</>
-		);
+import React from "react";
+import Frame from "../components/Frame";
+import Spacemotion from "../image/spacemotionn.mp4";
+import AnimatedGif from "../components/AnimatedGif ";
+import titletext from "../image/titletext.png";
+function Chamcham() {
+  return (
+    <>
+      {/* <Proverb /> */}
+      <Frame color={"#000000"} />
+      <div style={{ height: 330 }}></div>
+      <div style={{ textAlign: "center" }}>
+        <div
+          style={{
+            width: 837,
+            height: 373,
+            backgroundImage: `url(${titletext})`,
+            marginLeft: 100,
+            color: "black",
+          }}
+        >
+          dsfdsfsfsdfsdsdfdsf
+        </div>
+        {/* <AnimatedGif /> */}
+      </div>
+      <video autoplay="autoplay" muted="muted" loop="loop" controls>
+        <source src={Spacemotion} />
+      </video>
+    </>
+  );
 }
 export default Chamcham;
