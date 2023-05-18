73d3ceaeb89c1489a33068d252d78dadfd2602e1
youjin <w2117@e-mirim.hs.kr>
2023-05-18 10:21:48 +0900

    Add: Typing 효과

--- src/pages/Chamcham.jsx
+++ src/pages/Chamcham.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Frame from "../components/Frame";
 import styled from "styled-components";
-import TypeWriterEffect from "react-typewriter-effect";
+import { useNavigate } from "react-router-dom";
 
+// 비디오 스타일을 정의한 객체
 const videoStyle = {
   width: "838px",
   height: "1860px",
@@ -10,43 +11,117 @@ const videoStyle = {
   justifyContent: "center",
   alignItems: "center",
   margin: "0 auto",
+  marginLeft: "117px",
 };
 
 function Chamcham() {
+	// useNavigate 훅을 사용하여 navigate 함수를 받아옴
+  const navigate = useNavigate();
+	// "/" 경로로 이동하는 함수
+  const handleSkip = () => {
+    navigate("/");
+  };
+
+	// 초기 텍스트 상태를 설정하는 useState 훅
+  const [text, setText] = useState(
+    "오류..발생 ... 오류 발..zz 생 현재 비확인 물체가 뿜어낸 전자파로 인해 우주선 운행 프로그램에 오류가 발생했다. 대원들은 신속하게 수동으로 우주선을 조종해 비확인 물체로부터 우주선을 보호해라."
+  );
+
+	// useRef 훅을 사용하여 typingTextRef라는 변수 생성
+  const typingTextRef = useRef(null);
+
+  const handleTextChange = () => {
+  const newText = `우주선의 운행 방식은 아래와 같다.\n
+										1. 대원의 몸을 화면에 인식.\n
+										2. 화면에 선택에 따라, 오른쪽, 왼쪽 방향에 따라 몸을 움직이면서 랜덤으로 이동하고 싶은 방향으로 이동`;
+  // 텍스트를 변경하는 함수
+	setText(newText);
+	// typingTextRef의 resetTyping 함수 호출
+	// // typingTextRef의 resetTyping 함수 호출
+  typingTextRef.current.resetTyping();
+};
+
+
   return (
     <>
-      {/* <Proverb /> */}
+      <Container>
+        <Box>
+          <TypingText text={text} ref={typingTextRef} />
+          <Text onClick={handleTextChange}>skip</Text>
+        </Box>
+        <video style={videoStyle} autoPlay muted loop>
+          <source src="images/spacemotion.mp4" />
+        </video>
+      </Container>
       <Frame color={"#000000"} />
-      {/* <Videos autoplay muted loop>
-        <source src="images/spacemotion.mp4" />
-      </Videos> */}
-      <video style={videoStyle} autoplay="autoplay" muted="muted" loop="loop">
-        <source src="images/spacemotion.mp4" />
-      </video>
-      <Box>
-        {/* 오류..발생 ... 오류 발..zz 생<br />
-          현재 비확인 물체가 뿜어낸 전자파로 <br />
-          인해 우주선 운행 프로그램에 오류가 <br />
-          발생했다. 대원들은 신속하게 수동으로 <br />
-          우주선을 조종해 비확인 물체로부터<br />
-          우주선을 보호해라. */}
-        <TypeWriterEffect
-          startDelay={100}
-          // style속성은 textStyle 속성에
-          textStyle={{ fontSize: "2.5rem", padding: "8rem" }}
-          cursorColor="black"
-          text="오류..발생 ... 오류 발..zz 생
-                        현재 비확인 물체가 뿜어낸 전자파로 
-                        인해 우주선 운행 프로그램에 오류가 
-                        발생했다. 대원들은 신속하게 수동으로 
-                        우주선을 조종해 비확인 물체로부터
-                        우주선을 보호해라."
-          typeSpeed={100}
-        />
-      </Box>
     </>
   );
 }
+
+const TypingText = React.forwardRef(({ text }, ref) => {
+	// Text 상태 변수를 생성하고 초기값을 빈 문자열로 설정
+  const [Text, setText] = useState("");
+	// Count 상태 변수를 생성하고 초기값을 0으로 설정
+  const [Count, setCount] = useState(0);
+
+  useEffect(() => {
+		// 컴포넌트가 렌더링될 때마다 실행되는 부수 효과 함수
+    const interval = setInterval(() => {
+			// 0.1초마다 실행되는 인터벌 함수
+
+			// 이전 텍스트에 새로운 글자를 추가하여 Text 상태를 업데이트
+      setText((prevText) => prevText + text[Count]);
+			// Count 상태를 1 증가시킴
+      setCount((prevCount) => prevCount + 1);
+    }, 100);
+
+
+		 // Count가 텍스트의 길이와 같아지면
+    if (Count === text.length) {
+			// 인터벌 함수를 중지시킴
+      clearInterval(interval);
+    }
+
+		// 컴포넌트가 사라질 때 인터벌 함수를 정리(cleanup)
+    return () => clearInterval(interval);
+		// Count와 text가 변경될 때마다 부수 효과 함수가 실행
+  }, [Count, text]);
+
+
+  useEffect(() => {
+		 // 컴포넌트가 렌더링될 때와 ref가 변경될 때마다 실행되는 부수 효과 함수
+    if (ref) {
+      ref.current = {
+        resetTyping: () => {
+          setText(""); // Text 상태를 초기화
+          setCount(0);  // Count 상태를 초기화
+        },
+      };
+    }
+  }, [ref]); // ref가 변경될 때마다 부수 효과 함수가 실행
+
+  return (
+    <div>
+			{/* 텍스트가 출력될 곳 */}
+      <p className="text" style={{ fontSize: "2.5rem", padding: 75}}>
+        {Text}
+      </p>
+    </div>
+  );
+});
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  z-index: 100;
+`;
+
 const Box = styled.div`
   position: absolute;
   width: 837px;
@@ -62,5 +137,18 @@ const Box = styled.div`
   background-size: cover;
   font-size: 40px;
   font-weight: 400;
+  z-index: 999;
 `;
+
+const Text = styled.button`
+  position: absolute;
+  font-size: 45px;
+  background-color: #fafafa;
+  border: none;
+  color: #8b8b8b;
+  left: 670px;
+  top: 280px;
+  z-index: 6;
+`;
+
 export default Chamcham;
