4bcecac69474c6b343dcadf57b575e650db60dc2
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-05-23 00:11:18 +0900

    Fixed: mongoDB 연동 중 env변수 오류

--- server/server.js
+++ server/server.js
@@ -12,6 +12,14 @@ MongoClient.connect(
   `mongodb+srv://emirim:${process.env.MONGO_PASSWORD}@cluster0.ixtjj8v.mongodb.net/?retryWrites=true&w=majority`,
   function (에러, client) {
     if (에러) return console.log(에러);
+    db = client.db("voiceflow");
+
+    db.collection("score").insertOne(
+      { name: "John", phone: "01012341234", score: 538 },
+      (error, result) => {
+        console.log("저장완료");
+      }
+    );
     //서버띄우는 코드 여기로 옮기기
     app.listen("8080", function () {
       console.log("listening on 8080");
