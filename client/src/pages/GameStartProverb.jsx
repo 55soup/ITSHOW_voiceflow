f0fba3251c9ccc32be5630475fd304426eb62386
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-06-14 22:56:40 +0900

    Fix: 속담추가, 타이머추가, 그림추가, 점수조정

--- client/src/pages/GameStartProverb.jsx
+++ client/src/pages/GameStartProverb.jsx
@@ -6,7 +6,6 @@ import Timer from "../components/Timer";
 import Alien from "../components/Alien";
 
 function GameStartProverb() {
-
   const proverbFront = [
     "가는 날이",
     "아닌 밤중에",
@@ -35,6 +34,17 @@ function GameStartProverb() {
     "백지장도",
     "티끌모아",
     "금강산도",
+    "자다가",
+    "우물 에서",
+    "가랑비에",
+    "쥐구멍에도",
+    "닭 쫓던 개",
+    "사공이 많으면 배가",
+    "될 성 부른 나무는",
+    "벼는 익을 수록",
+    "하룻강아지",
+    "달면 삼키고",
+    "어물전 망신은",
   ];
   const proverbBack = [
     "장날이다",
@@ -64,19 +74,31 @@ function GameStartProverb() {
     "맞들면 낫다",
     "태산",
     "식후경",
+    "봉창 두드린다",
+    "숭늉 찾는다",
+    "옷 젖는 줄 모른다",
+    "볕 들 날이 있다",
+    "지붕 쳐다본다",
+    "산으로 간다",
+    "떡잎부터 알아본다",
+    "고개를 숙인다",
+    "범 무서운 줄 모른다",
+    "쓰면 뱉는다",
+    "꼴뚜기가 시킨다",
   ];
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(""); // 음성인식 텍스트 
   const { listen, listening, stop } = useSpeechRecognition({
     onResult: (result) => {
       // 음성인식 결과가 value 상태값으로 할당됩니다.
       setValue(result);
     },
   });
-  let [idx, setIdx] = useState(0);
-  let [count, setCount] = useState(0);
+  let [idx, setIdx] = useState(Math.floor(Math.random() * (proverbFront.length - 1)) + 1); // 문제 index
+  let [count, setCount] = useState(0); // 맞춘 갯수 -> 점수
   let [correct, setCorrect] = useState(false);
   // toggle버튼 관리
   const [toggle, setToggle] = useState(false);
+  const [passCount, setPassCount] = useState(0); // 패스 갯수
 
   useEffect(() => {
     if (proverbBack[idx] === value) {
@@ -100,7 +122,7 @@ function GameStartProverb() {
           }
           return nextIdx;
         });
-        setCount((prevCount) => prevCount + 1);
+        setCount((prevCount) => (prevCount + 1)+(Math.round(Math.random() * 100)));
         setCorrect(false); // 맞춘 후 다시 correct -> false로
       }, 2000);
       // 이전에 등록한 timeout을 clear 하기 위해 return 하여 clear 함수를 등록합니다.
@@ -108,26 +130,50 @@ function GameStartProverb() {
     }
   }, [correct, proverbFront.length]);
 
+  // game 시작전 5초 타이머
+  const [gamestart, setGamestart] = useState(false);
+  const [time, setTime] = useState(5);
+  useEffect(()=>{
+    const timer = setInterval(() => {
+      setTime((prev) => prev - 1);
+    }, 1000);
+    if(time < 1) setGamestart(true); // 0초가 되면 게임 시작
+    return () => clearInterval(timer);
+  }, [time])
+
+  // 마이크 버튼
   const clickedToggle = () => {
     setToggle((prev) => !prev);
   };
 
   function toNext() {
+    setPassCount(passCount+1);
     setIdx(Math.floor(Math.random() * (proverbFront.length - 1)) + 1);
   }
 
   return (
     <>
       <Container>
-        {/* <Timer count={count} second={60} start={false}/> */}
-        <div style={{ color: "white", fontSize: "5rem" }}>score: {count}</div>
-        <img src="/images/proverb/ufo.png" alt="ufo"/>
+        {!gamestart ? 
+          <>
+            <Text style={{zIndex: '500', fontSize: '10rem'}}>{time}</Text>
+            <GameBackground />
+          </> : 
+          <Timer score={count} second={60} />}
+        <Text style={{position: 'relative', bottom: '20rem', right: '25rem'}}>score: {count}</Text>
+        <Text style={{position: 'relative', bottom: '25rem', left: '20rem'}}>pass 횟수: {passCount}/3</Text>
+        <img src="/images/proverb/ufo.png" alt="ufo" style={{marginTop: '-10rem'}}/>
         <Alien />
         <div style={{display: 'flex', gap: '10rem'}}>
           <img src="/images/proverb/rocket.png" alt="rocket"/>
           <img src="/images/proverb/rocket.png" alt="rocket"/>
           <img src="/images/proverb/rocket.png" alt="rocket"/>
         </div>
+        <SmallSpeechBubble>
+          <CorrectStyle style={correct ? { color: "green" } : { color: "red" }}>
+            {correct ? "맞았어!" : "땡"}
+          </CorrectStyle>
+        </SmallSpeechBubble>
         <SpeechBubble rotate={"rotate(180deg)"}>
           <SpeechText rotate={"rotate(180deg)"} padding={"13vw"}>
             {proverbFront[idx]}
@@ -138,26 +184,30 @@ function GameStartProverb() {
             {value}
           </SpeechText>
         </SpeechBubble>
-        <CorrectStyle style={correct ? { color: "green" } : { color: "red" }}>
-          {correct ? "맞았어!" : "땡"}
-        </CorrectStyle>
-        <RecordButton
-          onClick={() => {
-            clickedToggle();
-            !toggle ? listen({ interimResults: true }) : stop();
-          }}
-          toggle={toggle}
-        >
-          🎤
-        </RecordButton>
-        {listening && <div>음성인식 활성화 중</div>}
-        <button onClick={toNext}>></button>
+          <div style={{display: 'flex', gap: '5rem'}}>
+          <RecordButton
+            onClick={() => {
+              clickedToggle();
+              !toggle ? listen({ interimResults: true }) : stop();
+            }}
+            toggle={toggle}>🎤</RecordButton>
+          {/* pass 3번 넘게 하면 없어지게 */}
+          {passCount < 3 ? <PassBtn visibility={true} onClick={toNext}>PASS</PassBtn> : <PassBtn visibility={false} onClick={toNext}>PASS</PassBtn>}
+        </div>
       </Container>
       <Frame color={"var(--background-main-color)"} />
     </>
   );
 }
-
+const GameBackground = styled.div`
+  width: 76%;
+  height: 73vh;
+  top: 18rem;
+  position: absolute;
+  background: black;
+  opacity: 0.5;
+  z-index: 300;
+`;
 const Container = styled.div`
   width: 100%;
   height: 100vh;
@@ -168,7 +218,10 @@ const Container = styled.div`
   align-items: center;
   z-index: 100;
 `;
-
+const Text = styled.div`
+  font-size: 5rem;
+  color: white;
+`
 const SpeechBubble = styled.div`
   width: 200vw;
   height: 22vw;
@@ -180,13 +233,11 @@ const SpeechBubble = styled.div`
   display: flex;
   justify-content: center;
 `;
-
 const SpeechText = styled.div`
   padding-top: ${(props) => props.padding};
   font-size: 4.5vw;
   transform: ${(props) => props.rotate};
 `;
-
 const RecordButton = styled.button`
   width: 10vw;
   height: 4vw;
@@ -194,11 +245,25 @@ const RecordButton = styled.button`
   border-radius: 5rem;
   border: 0;
 `;
-
 const CorrectStyle = styled.div`
-  font-size: 5rem;
+  font-size: 4rem;
   font-weight: 800;
   color: green;
+  margin-top: 8.3rem;
+`;
+const SmallSpeechBubble = styled(SpeechBubble)`
+  object-fit: contain;
+  position: absolute;
+  bottom: 100rem;
+  left: 15rem;
+  width: 30rem;
+`;
+const PassBtn = styled.button`
+  font-size: 5rem;
+  color: white;
+  cursor: pointer;
+  background: transparent;
+  visibility: ${(props)=> ( props.visibility ? "visible" : "hidden")};
 `;
 export default GameStartProverb;
 
